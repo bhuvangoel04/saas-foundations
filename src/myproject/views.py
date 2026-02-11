d8a623a41bdebd80a4ee870712052e19e8e83513
Bhuvan Goel <156914582+bhuvangoel04@users.noreply.github.com>
2026-02-11 16:28:26 +0000

    Page visits management added

--- src/myproject/views.py
+++ src/myproject/views.py
@@ -1,11 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from visits.models import PageVisit
 
 def home_page_view(request, *args, **kwargs):
     # return HttpResponse("<h1>Working</h1>")
+    path=request.path
+    print(path)
+    PageVisit.objects.create(path=request.path)
+    qs = PageVisit.objects.all() # The all() method returns a QuerySet of all the objects in the database.
+    page_qs = PageVisit.objects.filter(path=request.path)
     my_title = "Home page"
     html_template = "home.html"
     mycontext = {
-        "page_title": my_title
+        "page_title": my_title,
+        "total_page_visits": qs.count(),
+        "percent": ((page_qs.count()*100)/qs.count()),
+        "page_visit_count": page_qs.count(), # counts all the page visit objects created i.e. is no. of page visits
     }
     return render(request, html_template, mycontext)
--- src/visits/models.py
+++ src/visits/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 class PageVisit(models.Model):
     # id ->hidden ->autofield Auto incremented
-    path = models.TextField()
+    path = models.TextField(blank=True, null=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     
