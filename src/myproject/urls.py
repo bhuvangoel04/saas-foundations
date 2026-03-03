bad892b4e446fd8e38ac88d3dfd79541de9d294e
Bhuvan Goel <156914582+bhuvangoel04@users.noreply.github.com>
2026-03-03 20:13:20 +0000

    Added login view

--- src/myproject/urls.py
+++ src/myproject/urls.py
@@ -18,9 +18,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .views import home_page_view
+from auth import views as auth_views
 
 urlpatterns = [
     path("",home_page_view),
+    path("login/", auth_views.login_view),
     path("admin/", admin.site.urls),
     path("working/",home_page_view),
+    
 ]
