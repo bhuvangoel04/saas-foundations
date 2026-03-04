1445aaddaa4f2d7c8ae77b95cf7a2fe295e8cbbe
Bhuvan Goel <156914582+bhuvangoel04@users.noreply.github.com>
2026-03-04 18:28:54 +0000

    Added Registration

--- src/myproject/urls.py
+++ src/myproject/urls.py
@@ -23,6 +23,7 @@ from auth import views as auth_views
 urlpatterns = [
     path("",home_page_view),
     path("login/", auth_views.login_view),
+    path("register/", auth_views.register_view),
     path("admin/", admin.site.urls),
     path("working/",home_page_view),
     
