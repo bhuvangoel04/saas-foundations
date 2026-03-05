658cfd600dffe13dc94cddd0591663738e4a4ec3
Bhuvan Goel <156914582+bhuvangoel04@users.noreply.github.com>
2026-03-05 19:15:41 +0000

    Added navbar links

--- src/myproject/urls.py
+++ src/myproject/urls.py
@@ -21,7 +21,7 @@ from .views import home_page_view
 from auth import views as auth_views
 
 urlpatterns = [
-    path("",home_page_view),
+    path("",home_page_view, name="home"),
     path("login/", auth_views.login_view),
     path("register/", auth_views.register_view),
     path("working/",home_page_view),
